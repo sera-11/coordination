8bfd366e98fe516e7db9e9d926cd193a37781f17
Sera Oraha <72953661+sera-11@users.noreply.github.com>
2024-04-01 18:18:36 +0000

    created and migrated organizations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_01_172549) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_01_172932) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -52,6 +52,12 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_01_172549) do
   create_table "meeting_minutes", force: :cascade do |t|
     t.date "meeting_date"
     t.integer "organization_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "organizations", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
